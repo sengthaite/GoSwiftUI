6f4de502783091a6f362defedf3ad76920c2b024
Sengthai <088sengthai@gmail.com>
2022-09-15 09:09:52 +0700

    Initial Commit

---
+++ GoSwift/GoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoSwiftApp.swift
+//  GoSwift
+//
+//  Created by sengthaite on 15/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
